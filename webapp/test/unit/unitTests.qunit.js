069bb584890638dfd2ae42ec7f705768af979560
axelmz <axel.melendez@alumnos.udg.mx>
2021-11-04 11:01:50 -0600

    27: Unit test with QUnit

--- webapp/test/unit/unitTests.qunit.js
+++ webapp/test/unit/unitTests.qunit.js
@@ -5,7 +5,8 @@ sap.ui.getCore().attachInit(function () {
 	"use strict";
 
 	sap.ui.require([
-		"walkthrough_project/test/unit/AllTests"
+		//"walkthrough_project/test/unit/AllTests"
+		"walkthrough_project/test/unit/model/formatter"
 	], function () {
 		QUnit.start();
 	});
